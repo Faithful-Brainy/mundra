4846ce2e74a2fcce7722ff576895c92544caae74
FaithBB <fcdbbrainy@gmail.com>
2026-04-01 22:29:05 +0100

    prisma client error fix

--- app/api/controllers/updateUser/route.ts
+++ app/api/controllers/updateUser/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma-client";
-import { User } from "@prisma/client";
+import type { User } from "@prisma/client";
 import { NextResponse } from "next/server";
 import  jwt  from "jsonwebtoken";
 
