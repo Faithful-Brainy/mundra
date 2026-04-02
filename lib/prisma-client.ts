a317ff8407664efd7bbe9bc9072ffd721503f478
FaithBB <fcdbbrainy@gmail.com>
2026-04-02 10:56:26 +0100

    final prisma client error fix

--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -1,12 +1,14 @@
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
-const prisma = globalThis.prisma ?? new PrismaClient({adapter});
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
+const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
