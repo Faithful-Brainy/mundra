eadf711ebc0423f4517fa953e70ca338f4c6b180
FaithBB <fcdbbrainy@gmail.com>
2026-04-04 21:50:23 +0100

    mailing update 2

--- lib/email.ts
+++ lib/email.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function Mail(to: string, subject: string, html: string) {
     await resend.emails.send({
-        from: "Mundra Website <onboarding@resend.dev>",
+        from: "onboarding@resend.dev",
         to: "fcdbbrainy@gmail.com",
         subject,
         html,
