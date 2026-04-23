84f3d4cdf30de9be97772a188f2f1c8ebe164839
FaithBB <fcdbbrainy@gmail.com>
2026-04-23 19:38:42 +0100

    admission update

--- app/api/getAllFromTable/route.ts
+++ app/api/getAllFromTable/route.ts
@@ -1,47 +1,13 @@
 import prisma from "@/lib/prisma-client";
 import { NextResponse } from "next/server";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: Request) {
-    const payload: { tableName: string } = (await req.json()) as { tableName: string };
+    const payload = (await req.json()) as { tableName: string };
 
-    const tableName = (payload.tableName ?? "").trim();
+    const tableName = payload.tableName;
 
-    try {
-        if (tableName === "Subject") {
-            const data = await prisma.subject.findMany({
-                select: {
-                    id: true,
-                    name: true,
-                },
-                orderBy: {
-                    id: "asc",
-                },
-            });
+    const data = await prisma.$queryRaw`SELECT * FROM ${Prisma.raw(tableName)}`
 
-            return NextResponse.json(data);
-        }
-
-        if (tableName === "Class") {
-            const data = await prisma.class.findMany({
-                select: {
-                    id: true,
-                    name: true,
-                    level: true,
-                    subLevel: true,
-                },
-                orderBy: [
-                    { level: "asc" },
-                    { subLevel: "asc" },
-                    { name: "asc" },
-                ],
-            });
-
-            return NextResponse.json(data);
-        }
-
-        return NextResponse.json({error: "Unsupported table"}, {status: 400});
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json({error: "Database Connection Error"}, {status: 500})
-    }
+    return NextResponse.json(data);
 }
